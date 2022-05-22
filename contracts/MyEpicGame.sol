97bf539bdb3e299aa9b705ccd5ecdcffef87267e
Jakub Szczepanek <kubaszczepanek@interia.pl>
2022-05-22 19:43:48 +0200

    spaceships

--- contracts/MyEpicGame.sol
+++ contracts/MyEpicGame.sol
@@ -5,7 +5,42 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract MyEpicGame {
-    constructor() {
-        console.log("THIS IS MY GAME CONTRACT. NICE");
+    struct SpaceshipAttributes {
+        uint256 spaceshipIndex;
+        string name;
+        string imageURI;
+        uint256 hp;
+        uint256 maxHp;
+        uint256 attackDamage;
+    }
+
+    SpaceshipAttributes[] defaultSpaceships;
+
+    constructor(
+        string[] memory spaceshipNames,
+        string[] memory spaceshipImageURIs,
+        uint256[] memory spaceshipHp,
+        uint256[] memory spaceshipAttackDmg
+    ) {
+        for (uint256 i = 0; i < spaceshipNames.length; i += 1) {
+            defaultSpaceships.push(
+                SpaceshipAttributes({
+                    spaceshipIndex: i,
+                    name: spaceshipNames[i],
+                    imageURI: spaceshipImageURIs[i],
+                    hp: spaceshipHp[i],
+                    maxHp: spaceshipHp[i],
+                    attackDamage: spaceshipAttackDmg[i]
+                })
+            );
+
+            SpaceshipAttributes memory c = defaultSpaceships[i];
+            console.log(
+                "Done initializing %s w/ HP %s, img %s",
+                c.name,
+                c.hp,
+                c.imageURI
+            );
+        }
     }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,6 +1,13 @@
 const main = async () => {
     const gameContractFactory = await hre.ethers.getContractFactory('MyEpicGame');
-    const gameContract = await gameContractFactory.deploy();
+    const gameContract = await gameContractFactory.deploy(
+        ["Battleship", "Destroyer", "Bomber"],
+        ["http://wiki.ogame.org/images/thumb/0/0b/Ship_Battleship.jpg/180px-Ship_Battleship.jpg",
+        "http://wiki.ogame.org/images/thumb/1/14/Ship_Destroyer.jpg/180px-Ship_Destroyer.jpg",
+        "http://wiki.ogame.org/images/thumb/e/e3/Ship_Bomber.jpg/180px-Ship_Bomber.jpg"],
+        [200, 400, 800],
+        [200, 100, 50]
+    );
     await gameContract.deployed();
     console.log("Contract deployed to:", gameContract.address);
 };
