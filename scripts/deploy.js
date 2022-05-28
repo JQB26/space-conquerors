325f7505b10c3528cf9ee01b8480a0f21e69bea1
Jakub Szczepanek <kubaszczepanek@interia.pl>
2022-05-29 00:23:50 +0200

    rinkeby deploy

---
+++ scripts/deploy.js
@@ -0,0 +1,44 @@
+const main = async () => {
+    const gameContractFactory = await hre.ethers.getContractFactory('MyEpicGame');
+    const gameContract = await gameContractFactory.deploy(
+        ["Battleship", "Destroyer", "Bomber"],
+        ["http://wiki.ogame.org/images/thumb/0/0b/Ship_Battleship.jpg/180px-Ship_Battleship.jpg",
+        "http://wiki.ogame.org/images/thumb/1/14/Ship_Destroyer.jpg/180px-Ship_Destroyer.jpg",
+        "http://wiki.ogame.org/images/thumb/e/e3/Ship_Bomber.jpg/180px-Ship_Bomber.jpg"],
+        [200, 400, 800],
+        [200, 100, 50]
+    );
+    await gameContract.deployed();
+    console.log("Contract deployed to:", gameContract.address);
+
+    let txn;
+    txn = await gameContract.mintSpaceshipNFT(0);
+    await txn.wait();
+    console.log("Minted NFT #1");
+
+    txn = await gameContract.mintSpaceshipNFT(1);
+    await txn.wait();
+    console.log("Minted NFT #2");
+
+    txn = await gameContract.mintSpaceshipNFT(2);
+    await txn.wait();
+    console.log("Minted NFT #3");
+
+    txn = await gameContract.mintSpaceshipNFT(1);
+    await txn.wait();
+    console.log("Minted NFT #4");
+
+    console.log("Done deploying and minting!");
+};
+
+const runMain = async () => {
+    try {
+        await main();
+        process.exit(0);
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+};
+
+runMain();
